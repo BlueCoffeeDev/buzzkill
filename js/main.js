25fc62229e46cc274a9a91fc08e8068c584597fe
Joe <chanmew6@gmail.com>
2017-11-25 20:58:29 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Buzzkill was created by Joe Palmer, it is very early on InDev!</p>
+
+<button onclick="myFunction()">Lets begin!</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var person = prompt("Please enter your name", "Harry Potter");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+    }
+}
+</script>
+
+</body>
+</html>
 
